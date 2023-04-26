59006ce734b216f65090a94d30f89f4efb48871f
sjk338285@naver.com <sjk338285@gmail.com>
2023-04-26 10:10:23 +0900

    fb

--- squad_makers/lib/main.dart
+++ squad_makers/lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'view/start_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
